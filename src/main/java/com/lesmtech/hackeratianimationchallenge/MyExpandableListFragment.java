fcbf30772c59cf2cd139f5cbbe3d994af0f9ab13
Te <sheat2500@gmail.com>
2015-02-07 16:57:24 -0500

    RotationY Function

--- src/main/java/com/lesmtech/hackeratianimationchallenge/MyExpandableListFragment.java
+++ src/main/java/com/lesmtech/hackeratianimationchallenge/MyExpandableListFragment.java
@@ -1,22 +1,23 @@
 package com.lesmtech.hackeratianimationchallenge;
 
 import android.app.ActionBar;
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
-import android.widget.ExpandableListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.nhaarman.listviewanimations.appearance.simple.AlphaInAnimationAdapter;
-import com.nhaarman.listviewanimations.itemmanipulation.expandablelistitem.ExpandableListItemAdapter;
 
 import hugo.weaving.DebugLog;
 
-
-public class MainActivity extends Activity {
+/**
+ * Created by Te on 2/7/15.
+ */
+public class MyExpandableListFragment extends Fragment {
 
     private static final int INITIAL_DELAY_MILLIS = 500;
     private MyExpandableListItemAdapter mExpandableListItemAdapter;
@@ -24,13 +25,14 @@ public class MainActivity extends Activity {
     private ActionBar mActionBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mylist);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.activity_mylist, container, false);
 
-        mListView = (ListView) findViewById(R.id.activity_mylist_listview);
+        mListView = (ListView) rootView.findViewById(R.id.activity_mylist_listview);
 
-        mActionBar = getActionBar();
+        mActionBar = getActivity().getActionBar();
 
         mListView.setOnScrollListener(
                 new AbsListView.OnScrollListener() {
@@ -72,49 +74,22 @@ public class MainActivity extends Activity {
                 }
         );
 
-
-        mExpandableListItemAdapter = new MyExpandableListItemAdapter(this);
-
+        mExpandableListItemAdapter = new MyExpandableListItemAdapter(getActivity());
 
         AlphaInAnimationAdapter alphaInAnimationAdapter = new AlphaInAnimationAdapter(mExpandableListItemAdapter);
         alphaInAnimationAdapter.setAbsListView(mListView);
 
         assert alphaInAnimationAdapter.getViewAnimator() != null;
-        alphaInAnimationAdapter.getViewAnimator().
 
-                setInitialDelayMillis(INITIAL_DELAY_MILLIS);
+        alphaInAnimationAdapter.getViewAnimator()
+                .setInitialDelayMillis(INITIAL_DELAY_MILLIS);
 
         mListView.setAdapter(alphaInAnimationAdapter);
 
         mExpandableListItemAdapter.setLimit(1);
 
-        Toast.makeText(this, "Click any item to expand or collapse", Toast.LENGTH_LONG).
+        Toast.makeText(getActivity(), "Click any item to expand or collapse", Toast.LENGTH_LONG).show();
 
-                show();
-
+        return rootView;
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 }
---
+++ src/main/java/com/lesmtech/hackeratianimationchallenge/RotationYTransformer.java
@@ -0,0 +1,28 @@
+package com.lesmtech.hackeratianimationchallenge;
+
+import android.support.v4.view.ViewPager;
+import android.view.View;
+
+/**
+ * Created by Te on 2/7/15.
+ */
+public class RotationYTransformer implements ViewPager.PageTransformer {
+
+    @Override
+    public void transformPage(View view, float position) {
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0);
+
+        } else if (position <= 1) { // [-1,1]
+            // Modify the default slide transition to shrink the page as well
+
+            view.setAlpha(Math.abs(1 - Math.abs(position)));
+            view.setRotationY(180 * position);
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0);
+        }
+    }
+}
